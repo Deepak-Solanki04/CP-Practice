9f8bd65d275d37381dd608ed5875a426ea84a5a5
Deepak Solanki <singhdeepak8439@gmail.com>
2026-02-20 13:54:43 +0530

    Add CF_Problem37 solution for Arpa's exam problem

---
+++ Codeforces Problems/CF_Problem37 ( Arpa’s hard exam and Mehrdad’s naive cheat).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    if (n == 0) {
+        cout << 1;
+        return 0;
+    }
+
+    int cycle[4] = {8, 4, 2, 6};
+
+    cout << cycle[(n - 1) % 4];
+
+    return 0;
+}

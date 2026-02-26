3a5b3bc663379de106bcf4873c20605cf4e4078f
Deepak Solanki <singhdeepak8439@gmail.com>
2026-02-26 09:08:26 +0530

    Add CF_Problem43 to count occurrences of 'a'

---
+++ Codeforces Problems/CF_Problem43 (Count a's).cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    string s;
+    int q;
+    cin >> s >> q;
+ 
+    int n = s.size();
+    vector<int> prefix(n + 1, 0);
+ 
+    for(int i = 1; i <= n; i++) {
+        prefix[i] = prefix[i - 1];
+        if(s[i - 1] == 'a')
+            prefix[i]++;
+    }
+ 
+    while(q--) {
+        int L, R;
+        cin >> L >> R;
+        cout << prefix[R] - prefix[L - 1] << "\n";
+    }
+ 
+    return 0;
+}

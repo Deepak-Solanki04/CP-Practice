4a773452467449252a34e46255497920b94b6c0d
Deepak Solanki <singhdeepak8439@gmail.com>
2026-04-12 11:01:59 +0530

    Add CF_Problem84 (Homework) solution

---
+++ Codeforces Problems/CF_Problem84 (Homework).cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        string a;
+        cin >> a;
+ 
+        int m;
+        cin >> m;
+ 
+        string b, c;
+        cin >> b >> c;
+ 
+        deque<char> dq;
+ 
+        // put initial string
+        for (char ch : a) dq.push_back(ch);
+ 
+        for (int i = 0; i < m; i++) {
+            if (c[i] == 'V') {
+                dq.push_front(b[i]);
+            } else {
+                dq.push_back(b[i]);
+            }
+        }
+ 
+        for (char ch : dq) cout << ch;
+        cout << '\n';
+    }
+ 
+    return 0;
+}

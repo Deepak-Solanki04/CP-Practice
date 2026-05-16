38c90befc11d3d064536aefa3c995054549a0f03
Deepak Solanki <singhdeepak8439@gmail.com>
2026-05-16 23:28:14 +0530

    Add CF_Problem111 (Lawn Mover) solution

---
+++ Codeforces Problems/CF_Problem111 (Lawn Mover).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    cout<<(n-(n/m))<<endl;
+}
+ 
+int main(void){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}

d6490d078a5e7651993c838b953abdc2dc5e1858
Deepak Solanki <singhdeepak8439@gmail.com>
2026-06-22 09:53:18 +0530

    Add solution for CF Problem 131
    Implement solution for Codeforces Problem 131 (Same Difference).

---
+++ Codeforces Problems/CF_Problem131 (Same Difference).cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int count = 0;
+        for(int i = 0; i < n-1; i++){
+            if(s[i] != s[n-1]){
+                count ++;
+            }
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}

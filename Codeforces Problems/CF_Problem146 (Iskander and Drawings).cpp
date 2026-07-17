0b1af842fcd82c336c956032b5d675f3ad923e31
Deepak Solanki <singhdeepak8439@gmail.com>
2026-07-17 21:42:26 +0530

    Add CF_Problem146 solution for Iskander and Drawings

---
+++ Codeforces Problems/CF_Problem146 (Iskander and Drawings).cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n ;
+
+    string s;
+    cin >>s;
+
+    int mx = 0;
+    int curr = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(s[i] != '#')
+        {
+            curr=0;
+        }
+        else
+        {
+            curr++;
+        }
+
+        mx=max(mx,curr);
+
+    }
+
+    cout <<(mx+1)/2 << endl;
+
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+return 0;
+}
